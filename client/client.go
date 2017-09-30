3752e1b67585b4404bfe5af1b05d3617cbe49231
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-09-29 19:16:53 -0700

    also logging current time inchannels
    Former-commit-id: e428ea4a0a95a6b9380a7a8e266ae19a4615a4ae

--- client/client.go
+++ client/client.go
@@ -10,26 +10,35 @@ import (
 
 func makeRequest(url string, result chan<- string) {
 	start := time.Now()
-	statusCode, _, err := fasthttp.Get(nil, url)
+	statusCode, body, err := fasthttp.Get(nil, url)
+	// fasthttp.Get(nil, url)
 	if err != nil {
 		fmt.Printf("%v", err)
 	}
 	if statusCode != fasthttp.StatusOK {
 		fmt.Fprintf(os.Stderr, "Unexpected status code: %d. Expecting %d", statusCode, fasthttp.StatusOK)
+	}
+	// this is hard-coded
+	if len(body) != 27 {
+		fmt.Printf("%v", err)
 	}
 	getSecs := time.Since(start).Seconds()
 
-	statusCode, _, err = fasthttp.Post(nil, url, nil)
-	fasthttp.Post(nil, url, nil)
+	statusCode, body, err = fasthttp.Post(nil, url, nil)
+	// fasthttp.Post(nil, url, nil)
 	if err != nil {
 		fmt.Printf("%v", err)
 	}
 	if statusCode != fasthttp.StatusOK {
 		fmt.Fprintf(os.Stderr, "Unexpected status code: %d. Expecting %d", statusCode, fasthttp.StatusOK)
 	}
+	// this is hard-coded
+	if len(body) != 27 {
+		fmt.Printf("%v", err)
+	}
 	postSecs := time.Since(start).Seconds()
 
-	result <- fmt.Sprintf("GET: %f POST: %f", getSecs, postSecs)
+	result <- fmt.Sprintf("GET: %f POST: %f, at %v", getSecs, postSecs, time.Now().String())
 }
 
 func MakeRequestHelper(url string, result chan<- string, iterations int) {
@@ -43,8 +52,9 @@ func main() {
 	threadString := args[0]
 	iterationString := args[1]
 	url := args[2]
-	bufferString := args[3]
-	portString := args[4]
+	bufferString := args[4]
+	portString := args[3]
+	// port
 	threads, err := strconv.Atoi(threadString)
 	if err != nil {
 		fmt.Printf("%v", err)
@@ -57,29 +67,23 @@ func main() {
 	if err != nil {
 		fmt.Printf("%v", err)
 	}
-	port, err := strconv.Atoi(portString)
-	if err != nil {
-		fmt.Printf("%v", err)
-	}
-	url = fmt.Sprintf("%s%s", url, port)
-
+	// port, err := strconv.Atoi(portString)
+	// if err != nil {
+	// fmt.Printf("%v", err)
+	// }
+	url = fmt.Sprintf("%s:%s", url, portString)
 	buffers = 1
 	result := make(chan string, buffers)
 	resultSlice := make([]string, threads*iterations)
-
 	start := time.Now()
-
 	for i := 0; i < threads; i++ {
 		go MakeRequestHelper(url, result, iterations)
 	}
-
 	for i := 0; i < threads*iterations; i++ {
 		resultSlice[i] = <-result
 	}
-
 	fmt.Println("All threads took in total:", time.Since(start).Seconds())
 	fmt.Println("Time taken by each of the threads for GET and POST:")
-
 	for _, singleResult := range resultSlice {
 		fmt.Println(singleResult)
 	}
