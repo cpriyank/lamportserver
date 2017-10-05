fafc3c03e33390bb652e6343a77121990b1d433f
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-10-04 19:23:03 -0700

    Step 1 is go
    Former-commit-id: 622e437b3839e2273fef509c67473de7fda62743

--- server.go
+++ server.go
@@ -1,13 +1,47 @@
-package main
+package lamportserver
 
 import (
 	"fmt"
+	"github.com/buaazp/fasthttprouter"
 	"github.com/valyala/fasthttp"
+	"log"
 )
 
-func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Hi there! RequestURI is %q", ctx.RequestURI())
+// Index is the index handler
+func Index(ctx *fasthttp.RequestCtx) {
+	fmt.Fprint(ctx, "Welcome!\n")
 }
-func main() {
-	fasthttp.ListenAndServe(":8000", fastHTTPHandler)
+
+// Hello is the Hello handler
+func Hello(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "hello, %s!\n", ctx.UserValue("name"))
+}
+
+// MultiParams is the multi params handler
+func vertStats(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "hi, %s, %s!\n", ctx.UserValue("skierID"), ctx.UserValue("dayNum"))
+}
+
+// QueryArgs is used for uri query args test #11:
+// if the req uri is /ping?name=foo, output: Pong! foo
+// if the req uri is /piNg?name=foo, redirect to /ping, output: Pong!
+// func QueryArgs(ctx *fasthttp.RequestCtx) {
+// 	name := ctx.QueryArgs().Peek("stats")
+// 	fmt.Fprintf(ctx, "Pong! %s\n", string(stats))
+// }
+
+// MultiParams is the multi params handler
+func loadStats(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "hi, %s, %s %s %s %s!\n", ctx.UserValue("resortID"), ctx.UserValue("dayNum"), ctx.UserValue("skierID"), ctx.UserValue("liftID"), ctx.UserValue("timeStamp"))
+}
+
+func Serve() {
+	router := fasthttprouter.New()
+	router.GET("/", Index)
+	router.GET("/foo/:name", Hello)
+	router.GET("/myvert/:skierID/:dayNum", vertStats)
+	router.POST("/load/:resortID/:dayNum/:skierID/:liftID/:timeStamp", loadStats)
+	// router.POST("/load", QueryArgs)
+
+	log.Fatal(fasthttp.ListenAndServe(":8000", router.Handler))
 }
