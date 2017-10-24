827e35e6f08136b6abe180c182a20c7c028b1f97
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-10-24 01:45:02 -0700

    not using manipulate-stats.go anymore

--- manipulate-stats.go
+++ manipulate-stats.go
@@ -1,45 +1,32 @@
-package lamportserver
+// package lamportserver
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-const (
-	numSkiers = 40000
-	numDays   = 2
-	numLifts  = 40
-	maxInt    = 1 << 31
-)
+// const (
+// 	numSkiers = 40000
+// 	numDays   = 2
+// 	numLifts  = 40
+// 	maxInt    = 1 << 31
+// )
 
-func liftIDToVertical(liftID int) int {
-	switch {
-	case liftID <= 10:
-		return 200
-	case liftID <= 20:
-		return 300
-	case liftID <= 30:
-		return 400
-	case liftID <= 40:
-		return 500
-	}
-	return maxInt
-}
+// // this function is not called in recent git commit.
+// func mapSkierToDaysToLiftID(stats []*skierStat) ([][]int, [][]int) {
+// 	var skierLiftTable = make([][]int, numSkiers+1)
+// 	var skierVerticalTable = make([][]int, numSkiers+1)
 
-func mapSkierToDaysToLiftID(stats []*skierStat) ([][]int, [][]int) {
-	var skierLiftTable = make([][]int, numSkiers+1)
-	var skierVerticalTable = make([][]int, numSkiers+1)
+// 	// allocate skierTable
+// 	for i := range skierLiftTable {
+// 		skierLiftTable[i] = make([]int, numDays+1)
+// 		skierVerticalTable[i] = make([]int, numDays+1)
+// 	}
 
-	// allocate skierTable
-	for i := range skierLiftTable {
-		skierLiftTable[i] = make([]int, numDays+1)
-		skierVerticalTable[i] = make([]int, numDays+1)
-	}
-
-	for _, stat := range stats {
-		skierLiftTable[stat.skierID][stat.dayNum] += 1
-		skierVerticalTable[stat.skierID][stat.dayNum] += liftIDToVertical(stat.liftID)
-	}
-	fmt.Println(skierLiftTable)
-	fmt.Println(skierVerticalTable)
-	return skierLiftTable, skierVerticalTable
-}
+// 	for _, stat := range stats {
+// 		skierLiftTable[stat.skierID][stat.dayNum] += 1
+// 		skierVerticalTable[stat.skierID][stat.dayNum] += liftIDToVertical(stat.liftID)
+// 	}
+// 	fmt.Println(skierLiftTable)
+// 	fmt.Println(skierVerticalTable)
+// 	return skierLiftTable, skierVerticalTable
+// }
